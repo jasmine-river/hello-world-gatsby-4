67707254a2b8ef7db5dda6e63cf49f34967686e6
Jasmine Jiang <headlessnun@gmail.com>
2021-02-26 15:09:00 +0000

    Gatsby tutorial on google analytics / gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,19 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-NYK02X2QV1", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
 }
